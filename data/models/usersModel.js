c9a25539b93f3efdabfb4fec8c874ce968541323
Dominic Torruellas <dom@domtorr.com>
2019-08-22 11:56:58 -0700

    addUser method

--- data/models/usersModel.js
+++ data/models/usersModel.js
@@ -2,9 +2,18 @@ const db = require('../dbConfig');
 
 const getUsers = () => db('users');
 
-const getUserById = id => db('users').where({ id });
+const getUserById = id =>
+  db('users')
+    .where({ id })
+    .first();
+
+const addUser = user =>
+  db('users')
+    .insert(user)
+    .then(([id]) => getUserById(id));
 
 module.exports = {
   getUsers,
   getUserById,
+  addUser,
 };
