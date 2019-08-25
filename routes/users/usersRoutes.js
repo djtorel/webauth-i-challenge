b4604a0cbff9aa5ae585af873b20bfd323e209e8
Dominic Torruellas <dom@domtorr.com>
2019-08-24 20:37:46 -0700

    added session to login

--- routes/users/usersRoutes.js
+++ routes/users/usersRoutes.js
@@ -31,10 +31,15 @@ router.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const user = await getUserBy({ username });
-    user && bcrypt.compareSync(password, user.password)
-      ? res.status(200).json({ message: `Welcome ${user.username}` })
-      : res.status(401).json({ message: 'Invalid credentials' });
+    req.session.loggedIn = false;
+
+    const [user] = await getUserBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.loggedIn = true;
+      res.status(200).json({ message: `Welcome ${user.username}` });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
   } catch (err) {
     res.status(500).json({ message: 'Unable to login' });
   }
--- server.js
+++ server.js
@@ -2,13 +2,40 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+
 const apiRoutes = require('./routes');
+
+const SESSION_TIME = 1000 * 60 * 60;
+
+const sessionOptions = {
+  name: 'authCookie',
+  secret: 'ilikecookiestheyareyumyumdelicious',
+  cookie: {
+    maxAge: SESSION_TIME,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require('./data/dbConfig.js'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: SESSION_TIME,
+  }),
+};
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session(sessionOptions));
 
 server.use('/api', apiRoutes);
 
